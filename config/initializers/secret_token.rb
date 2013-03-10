a99443a6afaeef87c556ffeb916bacee295a4c92
JanaVPetrova <janavpetrova@gmail.com>
2013-03-10 23:02:40 +0400

    rebuild, registration form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DogOwner::Application.config.secret_token = '9ab5ca9d08ea733753f3addc5c11b403cc2fa78ef03b466ed24712b2ea54c75655fb34e383d7e557910b2e98573cf5c3ab8fac9d5fca40086e29eb3ea4c17365'
